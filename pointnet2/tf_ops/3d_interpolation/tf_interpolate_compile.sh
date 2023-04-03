40397c16997adfa7bf39a71f8608706b04c50a80
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-04-03 13:48:10 +0800

    Update tf_interpolate_compile.sh

--- pointnet2/tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ pointnet2/tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,6 +1,12 @@
-TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-CUDA_INC='/usr/local/cuda-11.8/include'
-CUDA_LIB='/usr/local/cuda-11.8/lib64/'
+#TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+#TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+#CUDA_INC='/usr/local/cuda-11.8/include'
+#CUDA_LIB='/usr/local/cuda-11.8/lib64/'
 
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -I $TF_INC -I $CUDA_INC -lcudart -L $CUDA_LIB -L $TF_LIB -ltensorflow_framework -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0 
+#g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -I $TF_INC -I $CUDA_INC -lcudart -L $CUDA_LIB -L $TF_LIB -ltensorflow_framework -shared -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0 
+
+
+TF_DIR=$(dirname $(python -c "import tensorflow as tf;print(tf.__file__)"))
+CUDA_HOME=/usr/local/cuda-11.8
+
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_DIR/include  -I ${CUDA_HOME}/include -lcudart -L ${CUDA_HOME}/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
