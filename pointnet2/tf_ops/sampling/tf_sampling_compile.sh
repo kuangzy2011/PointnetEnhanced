ee4ece3e07c1f346134428b2e851f4168700d4f8
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-04-03 13:53:49 +0800

    Update tf_sampling_compile.sh

--- pointnet2/tf_ops/sampling/tf_sampling_compile.sh
+++ pointnet2/tf_ops/sampling/tf_sampling_compile.sh
@@ -8,11 +8,10 @@
 
 #g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TF_INC -I $CUDA_INC -I $TF_INC/external/nsync/public -lcudart -L $CUDA_LIB -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
-
-/usr/local/cuda-11.5/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
-
 TF_DIR=$(dirname $(python -c "import tensorflow as tf;print(tf.__file__)"))
 CUDA_HOME=/usr/local/cuda-11.8
+
+${CUDA_HOME}/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 # TF1.2
 g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TF_DIR/include -I ${CUDA_HOME}/include -lcudart -L ${CUDA_HOME}/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
