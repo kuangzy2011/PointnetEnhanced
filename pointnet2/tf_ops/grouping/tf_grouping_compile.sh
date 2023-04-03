45cc71e3666d7350580bb798445193362f4cd484
Kuangzy <38204055+kuangzy2011@users.noreply.github.com>
2023-04-03 10:35:15 +0800

    Update tf_grouping_compile.sh

--- pointnet2/tf_ops/grouping/tf_grouping_compile.sh
+++ pointnet2/tf_ops/grouping/tf_grouping_compile.sh
@@ -3,7 +3,7 @@
 
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
-CUDA_INC='/usr/local/cuda-9.2/include'
-CUDA_LIB='/usr/local/cuda-9.2/lib64/'
+CUDA_INC='/usr/local/cuda-11.8/include'
+CUDA_LIB='/usr/local/cuda-11.8/lib64/'
 
 g++ -std=c++11 tf_grouping.cpp tf_grouping_g.cu.o -o tf_grouping_so.so -shared -fPIC -I $TF_INC -I $CUDA_INC -I $TF_INC/external/nsync/public -lcudart -L $CUDA_LIB -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
